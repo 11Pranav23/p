d5acfb9e5536b2bce43a267b7df5f3c5b2d0ca55
11Pranav23 <kumbharpranav24@gmail.com>
2024-06-26 11:00:53 +0530

    folder adding

---
+++ github upload/js3.java
@@ -0,0 +1,22 @@
+<html>
+<head></head>
+<body>
+<center>
+<h1>inheritance of class...</h1>
+
+<script>
+for(i=0;i<5;i++){
+var x=4;
+let y=5;
+document.write(x+y);
+
+document.write("<br>");
+}
+document.write(x+x);
+document.write(y+y);
+document.write(x+y);
+
+</script>
+</center>
+</body>
+</html> 
